4f309a1c7eb56d08db0fb49b5bb841a716ec0ec3
Eddy Jansson <eddy@klopper.net>
2020-08-07 17:29:48 +0200

    Add yaml format validation to build.
    Run 'yamllint' if available.

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+echo "Running tests"
 go test ./...
 if [ $? -ne 0 ]; then
 	echo "Test failures. Build aborted."
 	exit 1
 fi
+if [ -x "$(command -v yamllint)" ]; then
+	yamllint config/*.yaml || (echo "Configuration file error, aborted." && exit 2)
+	echo "Configuration file validation PASSED."
+else
+	echo "Configuration file validation skipped, 'yamllint' not available."
+fi
+echo "Building cmds..."
 go build ./cmd/words-cli
 go build ./cmd/words-server
+echo "Build done."
